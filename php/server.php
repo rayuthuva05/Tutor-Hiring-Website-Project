461222ba05f3970381b5f937e5577cde02f7d12b
rayuthuva05 <139629503+rayuthuva05@users.noreply.github.com>
2024-12-05 18:39:31 +0000

    db connected

--- php/server.php
+++ php/server.php
@@ -1,17 +1,19 @@
 <?php
-// Server credentials
-$servername = "XAMPP";
-$username = "root";
-$password = ""; // Default is empty for XAMPP
-$database = "";
-
+define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');
+define('DBNAME', 'e_teacher');
 try {
-    // Create connection
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // Set PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+	$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+	if (!$connect) {
+		die("connection failed".mysqli_connect_error());
+	} 
+	else {
+		//echo "Connected successfully<br>";
+	}
+} 
+catch (Exception $e) {
+	die($e->getMessage());
 }
+
 ?>
