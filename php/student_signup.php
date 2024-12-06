18efd014bc783a9e2cd538b7a4c73602b1372295
rayuthuva05 <139629503+rayuthuva05@users.noreply.github.com>
2024-12-06 17:38:47 +0000

    student register files are updated

---
+++ php/student_signup.php
@@ -0,0 +1,30 @@
+<?php
+	require_once 'dbconf.php';
+	function AddData($connect,$fullname,$sex,$age,$address,$phone,$gradien_phone,$school){
+		try {
+			$sql = "INSERT INTO learner VALUES('$fullname','$sex',$age,'$address','$phone','$gradien_phone','$school')";
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../register_next.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$fullname = $_POST['fullname'];
+		$sex = $_POST['sex'];
+		$age = $_POST['age'];
+		$address=$_POST['address'];
+		$phone=$_POST['phone'];
+		$gradien_phone=$_POST['guardian-phone'];
+		$school=$_POST['school'];
+		AddData($connect,$fullname,$sex,$age,$address,$phone,$gradien_phone,$school);
+	}
+
+	?>
