34dc6247225ffb9e0eec3ad2f6977b5edeb10ff7
rayuthuva05 <139629503+rayuthuva05@users.noreply.github.com>
2024-12-13 15:40:18 +0000

    in login php some changes are changed

--- php/login.php
+++ php/login.php
@@ -1,23 +1,21 @@
 <?php
+require_once 'dbconf.php';
 
-if (isset($_GET['id'])) {
-    $user = $_GET['user'];
+if (isset($_POST['user'])) {
+    $user = $_POST['user'];
 
     $sql = "SELECT * FROM user WHERE username = $user";
     
-
+    $result = $stmt->get_result();
     if ($result->num_rows > 0) {
         $user = $result->fetch_assoc();
-       // echo "Welcome, " . htmlspecialchars($user['name']) . "! You have successfully logged in.";
+         echo "Welcome, " . htmlspecialchars($user['name']) . "! You have successfully logged in.";
          header('Location: ../Students_dashboard.php'); 
          exit;
     } else {
         echo "User not found!";
     }
 
-    $stmt->close();
-} else {
-    echo "No username provided in the URL.";
 }
 
 $conn->close();
