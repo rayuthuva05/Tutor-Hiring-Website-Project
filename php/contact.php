dc6fcee5636f102b06bd0543873fb75f3d2e629b
rayuthuva05 <139629503+rayuthuva05@users.noreply.github.com>
2024-12-19 11:42:08 +0000

    contact php file created and table created for contact form

---
+++ php/contact.php
@@ -0,0 +1,28 @@
+<?php
+	require_once 'dbconf.php';
+	function AddData($connect,$name,$email,$phone,$reason,$message){
+		try {
+			$sql = "INSERT INTO contact_form VALUES('$name','$email','$phone','$reason','$message')";
+			$result = mysqli_query($connect,$sql);
+			if ($result) {
+				
+			} else {
+				die("Error ".mysqli_error($connect));
+			}
+            header('Location: ../#.html');
+            exit;
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+	
+	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$phone = $_POST['phone'];
+        $reason=$_POST['reason'];
+        $message=$_POST['message'];
+		AddData($connect,$name,$email,$phone,$reason,$message);
+	}
+
+	?>
